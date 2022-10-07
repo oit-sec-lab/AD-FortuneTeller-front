cf20a2b13da4444e30509158b98d2c3769b887e8
tami <tamio3141@outlook.jp>
2022-10-07 16:00:07 +0900

    fix valuename

--- content.js
+++ content.js
@@ -2,17 +2,17 @@ let URL = "http://127.0.0.1:5000/"
 
 var link_json_array = getURLs();
 
-checkGPC(URL, link_json_array)
+checkAD(URL, link_json_array)
     .then(res => res.json())
     .then(data => {
-        insert_gpc_icon(data);
+        insert_ad_icon(data);
     })
     .catch((error) => {
         console.error(error);
     });
 
 
-async function checkGPC(url, links) {
+async function checkAD(url, links) {
     console.log("links")
     console.log(links)
     console.log("url")
--- insert_icons.js
+++ insert_icons.js
@@ -1,15 +1,15 @@
 const EXTENSION_ID = chrome.runtime.id;
-const IMG_PATH_TRUE = 'chrome-extension://' + EXTENSION_ID + '/images/gpc_logo_true.jpg'
-const IMG_PATH_FLASE = 'chrome-extension://' + EXTENSION_ID + '/images/gpc_logo_false.jpg'
+const IMG_PATH_TRUE = 'chrome-extension://' + EXTENSION_ID + '/images/ad_logo_true.jpg'
+const IMG_PATH_FLASE = 'chrome-extension://' + EXTENSION_ID + '/images/ad_logo_false.jpg'
 const IMG_TAG_TRUE = "<img src=" + IMG_PATH_TRUE + ">"
 const IMG_TAG_FALSE = "<img src=" + IMG_PATH_FLASE + ">"
-function insert_gpc_icon(gpc_result) {
+function insert_ad_icon(ad_result) {
     var index = 1;
-    console.log(gpc_result);
+    console.log(ad_result);
     $('.yuRUbf a').each(function() {
         var URL = $(this).prop('href');
         if (URL.indexOf('webcache') == -1 && URL.indexOf('www.google.com/search?') == -1) {
-            var match_array_bool = gpc_result[index]['ad'];
+            var match_array_bool = ad_result[index]['ad'];
             console.log("yuRUbf a");
             if(!match_array_bool){
                 $(this).children("br").after(IMG_TAG_TRUE);
@@ -24,7 +24,7 @@ function insert_gpc_icon(gpc_result) {
         var URL = $(this).prop('href');
         if (URL.indexOf('webcache') == -1 && URL.indexOf('www.google.com/search?') == -1) {
             console.log(".yuRUbf link");
-            var match_array_bool = gpc_result[index]['ad'];
+            var match_array_bool = ad_result[index]['ad'];
             if(!match_array_bool){
                 $(this).children("br").after(IMG_TAG_TRUE);
             }
@@ -39,7 +39,7 @@ function insert_gpc_icon(gpc_result) {
         var URL = $(this).prop('href');
         if (URL.indexOf('webcache') == -1 && URL.indexOf('www.google.com/search?') == -1) {
             console.log("g-link link");
-            var match_array_bool = gpc_result[index]['ad'];
+            var match_array_bool = ad_result[index]['ad'];
             if(!match_array_bool){
                 $(this).children("br").after(IMG_TAG_TRUE);
             }
@@ -54,7 +54,7 @@ function insert_gpc_icon(gpc_result) {
         var URL = $(this).prop('href');
         if (URL.indexOf('webcache') == -1 && URL.indexOf('www.google.com/search?') == -1) {
             console.log("g-link a");
-            var match_array_bool = gpc_result[index]['ad'];
+            var match_array_bool = ad_result[index]['ad'];
             if(!match_array_bool){
                 $(this).children("br").after(IMG_TAG_TRUE);
             }
